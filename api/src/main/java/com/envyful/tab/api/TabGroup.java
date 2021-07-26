10b7d496a4c338f37e22da6948f7bd9200b304e7
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-26 07:50:07 +0100

    Feat: added tabgroup interface

---
+++ api/src/main/java/com/envyful/tab/api/TabGroup.java
@@ -0,0 +1,43 @@
+package com.envyful.tab.api;
+
+/**
+ *
+ * An interface representing a group of players on the tablist
+ *
+ */
+public interface TabGroup {
+
+    /**
+     *
+     * The weight (priority) of the tab group. The higher the weight the higher the priority (i.e. higher weights get
+     * put first)
+     *
+     * @return The weight
+     */
+    int getWeight();
+
+    /**
+     *
+     * Gets the permission required to be a member of this tab group
+     *
+     * @return The permission required
+     */
+    String getPermission();
+
+    /**
+     *
+     * Gets the tab group's prefix
+     *
+     * @return The prefix for the group
+     */
+    String getPrefix();
+
+    /**
+     *
+     * Gets the tab group's suffix
+     *
+     * @return The suffix for the tab group
+     */
+    String getSuffix();
+
+}
