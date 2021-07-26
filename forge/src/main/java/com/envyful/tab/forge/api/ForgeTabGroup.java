8ca40c56e526c2fcc648052619f45442370a8ac5
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-26 07:53:59 +0100

    Feat: added ForgeTabGroup

---
+++ forge/src/main/java/com/envyful/tab/forge/api/ForgeTabGroup.java
@@ -0,0 +1,44 @@
+package com.envyful.tab.forge.api;
+
+import com.envyful.tab.api.TabGroup;
+import org.spongepowered.configurate.ConfigurationNode;
+
+/**
+ *
+ * Forge implementation of the {@link TabGroup} interface
+ *
+ */
+public class ForgeTabGroup implements TabGroup {
+
+    private final int weight;
+    private final String permission;
+    private final String prefix;
+    private final String suffix;
+
+    public ForgeTabGroup(ConfigurationNode node) {
+        this.weight = node.node("weight").getInt();
+        this.permission = node.node("permission").getString();
+        this.prefix = node.node("prefix").getString();
+        this.suffix = node.node("suffix").getString();
+    }
+
+    @Override
+    public int getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public String getPermission() {
+        return this.permission;
+    }
+
+    @Override
+    public String getPrefix() {
+        return this.prefix;
+    }
+
+    @Override
+    public String getSuffix() {
+        return this.suffix;
+    }
+}
