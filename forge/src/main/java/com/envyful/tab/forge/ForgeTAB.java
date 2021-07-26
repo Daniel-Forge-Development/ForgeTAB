a98fc97eb2445312d5ff839afde6ae1309076d09
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-07-26 07:52:00 +0100

    Feat: added main mod

---
+++ forge/src/main/java/com/envyful/tab/forge/ForgeTAB.java
@@ -0,0 +1,44 @@
+package com.envyful.tab.forge;
+
+import com.envyful.api.forge.concurrency.ForgeUpdateBuilder;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import org.bstats.forge.Metrics;
+
+import java.nio.file.Paths;
+
+@Mod(
+        modid = "forgetab",
+        name = "Forge TAB",
+        version = ForgeTAB.VERSION,
+        acceptableRemoteVersions = "*"
+)
+public class ForgeTAB {
+
+    public static final String VERSION = "0.0.1";
+
+    @Mod.EventHandler
+    public void onServerStarting(FMLPreInitializationEvent event) {
+        Metrics metrics = new Metrics(
+                Loader.instance().activeModContainer(),
+                event.getModLog(),
+                Paths.get("config/"),
+                12198 //TODO
+        );
+
+        ForgeUpdateBuilder.instance()
+                .name("ForgeTAB")
+                .version(VERSION)
+                .requiredPermission("tab.update.notify")
+                .owner("Pixelmon-Development")
+                .repo("ForgeTAB")
+                .start();
+    }
+
+    @Mod.EventHandler
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
+}
