7cb6f804060891177f3670ef342af2e840880f99
Daniel Norris <dnorris443@gmail.com>
2022-01-13 22:10:59 +0000

    chore(gradle): bump version to 1.0.0

--- forge/src/main/java/com/envyful/tab/forge/ForgeTAB.java
+++ forge/src/main/java/com/envyful/tab/forge/ForgeTAB.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 )
 public class ForgeTAB {
 
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0";
 
     private static ForgeTAB instance;
 
