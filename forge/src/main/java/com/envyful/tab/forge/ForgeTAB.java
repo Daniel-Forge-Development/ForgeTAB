259926d51fb970f3076298649d21ad0f598258ca
Daniel Norris <dnorris443@gmail.com>
2023-09-10 10:52:03 +0100

    chore(gradle): update version to 1.1.2

--- forge/src/main/java/com/envyful/tab/forge/ForgeTAB.java
+++ forge/src/main/java/com/envyful/tab/forge/ForgeTAB.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 )
 public class ForgeTAB {
 
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
 
     private static ForgeTAB instance;
 
